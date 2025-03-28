865b737ec0e5f9f0c29c921baf040779438271ec
betxcircle <150256421+betxcircle@users.noreply.github.com>
2025-03-28 09:42:44 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import store from './redux/store.js'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/circles-web/'>
+  <BrowserRouter>
    <Provider store={store}>
     <App />
    </Provider>
